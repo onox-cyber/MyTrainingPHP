90dbaf07a9bb4f3ef2dc56c3adab8eb5b74d4a0b
onox <168278109+onox-cyber@users.noreply.github.com>
2026-01-18 15:31:28 +0900

    Update config.php

--- config.php
+++ config.php
@@ -1,6 +1,6 @@
 <?php
 try {
-    $db = new PDO("mysql:dbname=test;host=localhost;charset=utf8", "root", "f7HGp6y3ES-6xe");
+    $db = new PDO("mysql:dbname=test;host=localhost;charset=utf8", "root", "root");
 } catch(PDOExcenption $e) {
     echo $e->getMessage();
     exit();
